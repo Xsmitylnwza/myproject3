90378d379908ff9b62b3177d188aa1ca55ec4044
Chaimongkon Sokgampang <golfpopmei14@gmail.com>
2023-03-21 20:42:26 +0700

    Revert "new version2"
    This reverts commit e92b61c2efe8ebed3275bb83e6403adcabe49113.

--- Myproject3.java
+++ Myproject3.java
@@ -2,6 +2,5 @@ public class Myproject3 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello Java");
-        System.out.println("Hello MyGit za");
     }
 }
